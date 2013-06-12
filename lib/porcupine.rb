731dc63f160e62fa59a06f2a06474df89c424f0d
Dglgmut <florez.renan@gmail.com>
2013-06-12 00:36:59 -0300

    adding dirty monkeys to the code

--- lib/porcupine.rb
+++ lib/porcupine.rb
@@ -1,2 +1,18 @@
 class Porcupine
 end
+
+require 'net/smtp'
+Net::SMTP.class_eval do
+  def self.start
+    self.new.start
+  end
+
+  def self.new
+    super(1)
+  end
+
+  def start
+    @started = true
+    return self
+  end
+end
--- test/test_porcupine.rb
+++ test/test_porcupine.rb
@@ -1,9 +1,18 @@
 require 'test/unit'
 require 'porcupine'
+require 'net/smtp'
 
 class PorcupineTest < Test::Unit::TestCase
   def test_initialize
     assert_equal Porcupine,
       Porcupine.new.class
+  end
+
+  def test_stop_smtp_sending
+    assert_nothing_raised do
+      Net::SMTP.new()
+    end
+    n = Net::SMTP.new()
+    assert_equal n.start.started?, true
   end
 end
